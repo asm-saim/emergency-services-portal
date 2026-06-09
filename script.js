1b04fb41ddb4d74682c6a1b1fd21d6efdf208a70
asm-saim <saim777m@gmail.com>
2026-06-09 23:44:56 +0600

    heart icon toggle added

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hi from script")
+//heart image color change
+const colorChange = document.getElementById("heart").addEventListener('click',
+    function () {
+        this.classList.toggle('fa-solid');
+        this.classList.toggle('fa-regular')
+        this.style.color = this.classList.contains('fa-solid') ? 'red' : "";
+    }
+)
