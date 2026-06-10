e1f0f4091ce54a9d968634f9384b54253c25bef8
asm-saim <saim777m@gmail.com>
2026-06-10 14:37:05 +0600

    cal button functionality implemented

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 //heart image color change and value increment
 let value = 0;
-const colorChange = document.getElementById("heart").addEventListener('click',
-    function () {
-        this.classList.toggle('fa-solid');
-        this.classList.toggle('fa-regular')
-        this.style.color = this.classList.contains('fa-solid') ? 'red' : "";
 
-        const heartAdd = document.getElementById('heart-add');
-        value++;
-        heartAdd.innerText = value;
-    }
-)
+const heartIcons = document.getElementsByClassName('heart');
+
+for (let heartIcon of heartIcons) {
+    heartIcon.addEventListener('click',
+        function () {
+            this.classList.toggle('fa-solid');
+            this.classList.toggle('fa-regular');
+            this.style.color = this.classList.contains('fa-solid') ? 'red' : "";
+
+            const heartAdd = document.getElementById("heart-add");
+            value++;
+            heartAdd.innerText = value
+        }
+    )
+}
+
 
 //Click on Card and 20 coins will reduce.
 const cards = document.getElementsByClassName('card-selection');
@@ -19,14 +25,13 @@ for (let card of cards) {
     card.addEventListener('click',
         function () {
 
-            // calling alert
-            const cardItems = document.getElementsByClassName("card-items");
-            for (let cardItem of cardItems) {
-                const serviceName = cardItem.querySelector('p').innerText;
-                const text = cardItem.querySelectorAll('h2');
-                const number = text[1].innerText;
-                alert(`Calling ${serviceName}\nNumber: ${number}`);
-            }
+            // ONLY this clicked card alert
+            const card = this.closest(".card");
+
+            const name = card.querySelector(".service-type").innerText;
+            const number = card.querySelector(".service-number").innerText;
+
+            alert(`Calling ${name}\nNumber: ${number}`);
 
 
             const coinValue = document.getElementById('coin-value');
